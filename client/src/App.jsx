7d13bd66dd23f5c723cfa1e3a43b408553f618c9
unknown <mafaigabriel@gmail.com>
2025-02-28 05:35:30 +0100

    src

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,10 +15,23 @@ import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
 import UnathPage from "./pages/unauth-page";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuth } from "./store/auth-slice";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect } from "react";
 
 function App() {
-  const isAuthenticated = false;
-  const user = null
+
+  const {user,isAuthenticated, isLoading} = useSelector(state=> state.auth)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+
+  console.log(isLoading, user);
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,5 +1,8 @@
 import CommonForm from "@/components/common/form";
 import { loginFormControls } from "@/config";
+import { useToast } from "@/components/ui/use-toast";
+import { useDispatch } from "react-redux";
+import { loginUser } from "@/store/auth-slice";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -13,9 +16,24 @@ const initialState = {
 function AuthLogin() {
 
   const [formData, setFormData] = useState(initialState)
+  const dispatch = useDispatch();
+  const { toast } = useToast();
 
-  function onSubmit() {
-    
+  function onSubmit(event) {
+    event.preventDefault();
+
+    dispatch(loginUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
   }
   
   return (
